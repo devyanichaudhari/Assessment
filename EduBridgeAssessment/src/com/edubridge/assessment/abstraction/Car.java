7249b746d907ed5ce30e1349887ba5203d8c986e
Hemant <Hemant@LAPTOP-G60O6SHK>
2024-02-24 22:04:10 +0530

    commit

--- EduBridgeAssessment/src/com/edubridge/assessment/abstraction/Car.java
+++ EduBridgeAssessment/src/com/edubridge/assessment/abstraction/Car.java
@@ -3,4 +3,16 @@ package com.edubridge.assessment.abstraction;
 public class Car extends Vehicle
 {
 
+	@Override
+	void startEngine() {
+		System.out.println("Car Engine is started");
+		
+	}
+
+	@Override
+	void stopEngine() {
+		System.out.println("Car Engine is stopped");
+		
+	}
+
 }

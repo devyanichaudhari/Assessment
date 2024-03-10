5e0bc1a3006128ad7ceebf130c73bf0fead35534
Hemant <Hemant@LAPTOP-G60O6SHK>
2024-03-10 15:16:42 +0530

    commit

--- RestaurantManagement/src/com/eb/dao/OrderDao.java
+++ RestaurantManagement/src/com/eb/dao/OrderDao.java
@@ -1,5 +1,49 @@
 package com.eb.dao;
 
+import java.sql.*;
+import com.eb.database.CP;
 public class OrderDao {
+
+	
+	/*public static void display() 
+	{
+		//pending display
+		
+		try 
+		{
+		Connection conn = CP.createc();
+		String sql = "select * from orders";
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		System.out.println("Order ID\t"+"Order totalBill");
+			while(rs.next())
+			{
+				System.out.println(rs.getInt(1)+rs.getInt(2));
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+*/
+	
+	public static boolean delete(int oid)
+	{
+		try
+		{
+		Connection conn = CP.createc();
+		String sql2 = "delete from Orders where oid = ? ";
+		PreparedStatement ps = conn.prepareStatement(sql2);
+		ps.setInt(1, oid);
+		ps.executeUpdate();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
 
 }
